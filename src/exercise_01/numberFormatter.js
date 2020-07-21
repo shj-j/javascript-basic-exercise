5cb88706826177e503995c4b290ab63cfe327475
shj-j <shang.jie@outlook.com>
2020-07-21 17:46:23 +0800

    [sj]: add ex1

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,5 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  return option ? "$ "+ number.toFixed(2) : number.toFixed(2);
 }
