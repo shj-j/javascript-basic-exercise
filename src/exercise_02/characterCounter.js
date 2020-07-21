68a0ae1dd2a89de9680cd52258f593ce7b275afb
shj-j <shang.jie@outlook.com>
2020-07-21 18:57:00 +0800

    [sj]: add ex2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,15 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (string){
+    if(prediction){
+      let num = 0;
+      string.split("").reduce((pre,next) => { prediction(next) ? num++ : 0},[]);
+      return num;
+    }else{
+      return string.length;
+    }
+  }else{
+    return 0;
+  }
 }
